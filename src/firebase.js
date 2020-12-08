5cb8433785668ccd4fb944f4ea2a7218abb6f290
DR-Joey <jegal07@naver.com>
2020-12-08 23:37:42 +0900

    #1.1 dotenv solve

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,17 @@
 import firebase from "firebase/app";
 
+require("dotenv").config();
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD86asHNjZG6h5eifO6TO0QaBQJlOM07TI",
-  authDomain: "joeytwitt-e96c8.firebaseapp.com",
-  projectId: "joeytwitt-e96c8",
-  storageBucket: "joeytwitt-e96c8.appspot.com",
-  messagingSenderId: "626667276390",
-  appId: "1:626667276390:web:fa86d9c939197a359805a7",
-  measurementId: "G-GRC4VJ822L",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUR_ID,
 };
+console.log(firebaseConfig);
 
 export default firebase.initializeApp(firebaseConfig);
