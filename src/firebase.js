d2f803221f0e5cc6bd2ece32f038b24ff132aa0f
DR-Joey <jegal07@naver.com>
2020-12-08 23:05:28 +0900

    #1.1 Firebase SetUp

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD86asHNjZG6h5eifO6TO0QaBQJlOM07TI",
+  authDomain: "joeytwitt-e96c8.firebaseapp.com",
+  projectId: "joeytwitt-e96c8",
+  storageBucket: "joeytwitt-e96c8.appspot.com",
+  messagingSenderId: "626667276390",
+  appId: "1:626667276390:web:fa86d9c939197a359805a7",
+  measurementId: "G-GRC4VJ822L",
+};
+
+export default firebase.initializeApp(firebaseConfig);
